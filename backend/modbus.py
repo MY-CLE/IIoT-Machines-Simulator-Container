09238b348486d0d4c43b05d94b407602180d817f
Enrico Belgiovine <disgame@gmx.de>
2023-04-27 18:01:59 +0200

    test(backend): new modbus library attempt

--- backend/modbus.py
+++ backend/modbus.py
@@ -1,5 +1,16 @@
-from pyModbusTCP.client import ModbusClient
+from pymodbus.client import ModbusTcpClient
 
-client = ModbusClient(host="192.168.178.60", port=502, debug=True) # Master sendet an Slave
-client.open()
-print(client.read_holding_registers(0,4))
+import random
+import time
+import logging
+
+logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', level=logging.DEBUG, encoding='utf-8')
+
+client = ModbusTcpClient(host="192.168.178.60", port=502, debug=True) # Master sendet an Slave
+logging.info(f'Connection established: {client.connect()}')
+client.write_coil(0x01, True)
+
+client.read_coils(0x01)
+result = client.read_input_registers(64)
+print(result)
+client.close()
