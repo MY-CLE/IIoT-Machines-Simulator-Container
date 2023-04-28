9ef40e857a522ea1d61afb2cbe7340561db721ae
Enrico Belgiovine <disgame@gmx.de>
2023-04-28 18:39:06 +0200

    test(backend): added test loop for all registers

--- backend/modbus.py
+++ backend/modbus.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 import os
 import time
+import random
 import logging
 
 logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', level=logging.DEBUG, encoding='utf-8')
@@ -20,18 +21,26 @@ class Modbus:
         else:
             logging.error(self.connError)
 
-    def writeSingleRegister(self, regAddr: int = 0, value: int = 0):
+    def writeSingleRegister(self, regAddr: int = 0, regValue: int = 0):
         if self.connEstablished:
-            wSR = self.client.write_single_register(regAddr, value)
-            logging.info(f'Write Single Register | Writing at register Address: {regAddr} --> {value}')
+            wSR = self.client.write_single_register(regAddr, regValue)
+            logging.info(f'Write Single Register | Writing at register Address: {regAddr} --> {regValue}')
             return wSR
         else:
             logging.error(self.connError)
 
-    def writeMultipleRegisters(self, regAddr: int = 0, value: int = 0):
+    def writeSingleCoil(self, bitAddr, bitValue):
         if self.connEstablished:
-            wMR = self.client.write_multiple_registers(regAddr, value)
-            logging.info(f'Write Single Register | Writing at register Address: {regAddr} --> {value}')
+            wSC = self.client.write_single_coil(bitAddr, bitValue)
+            logging.info(f'Write Single Coil | Writing at Coil Address: {bitAddr} --> {bitValue}')
+            return wSC
+        else:
+            logging.error(self.connError)
+
+    def writeMultipleRegisters(self, regAddr: int = 0, regValue: int = 0):
+        if self.connEstablished:
+            wMR = self.client.write_multiple_registers(regAddr, regValue)
+            logging.info(f'Write Single Register | Writing at register Address: {regAddr} --> {regValue}')
             return  wMR
         else:
             logging.error(self.connError)
@@ -54,10 +63,13 @@ class Modbus:
 
 if __name__ == '__main__':
     c = Modbus()
-    c.writeSingleRegister(2, 1510)
-
+    #c.writeSingleCoil(1, True)
     i = 0
-    while i < 2:
+    while i < 48:
+        c.writeSingleRegister(i, random.randint(0, 1500))
+        """ c.writeSingleRegister(21, random.randint(0, 1500))
+        c.writeSingleRegister(22, random.randint(0, 1500))
+        c.writeSingleRegister(25, random.randint(0, 1500)) """
         #readInputRegisters = client.read_input_registers(0, 48) # Max reg_nb: 48
         #readHoldingRegisters  = client.read_holding_registers(0, 48) # Max reg_nb: 48
         #if readInputRegisters:
@@ -65,8 +77,15 @@ if __name__ == '__main__':
             #print(f'Read Holding Registers: {readHoldingRegisters}')
         #else:
            #print('Read error occured')
-        c.readInputRegisters(2, 1)
-        time.sleep(2)
+        c.readInputRegisters(i, 1)
+        """ c.readInputRegisters(21, 1)
+        c.readInputRegisters(22, 1)
+        c.readInputRegisters(25, 1) """
+        c.readHoldingRegisters(i, 1)
+        """ c.readHoldingRegisters(21, 1)
+        c.readHoldingRegisters(22, 1)
+        c.readHoldingRegisters(25, 1) """
+        time.sleep(5)
         i = i + 1
     c.client.close()
     
