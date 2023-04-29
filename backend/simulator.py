7cc0cf1942dfb38969fdf8829982232bf01f94d8
UnknownOlaf <glaseryael@yahoo.com>
2023-04-29 13:30:37 +0200

    test(backend): added first functions

---
+++ backend/simulator.py
@@ -0,0 +1,56 @@
+import time
+import random
+import sys
+
+class Simulator: 
+    def __init__(self) -> None:
+        self.startTime = None
+        self.stopTime = None
+        self.runTime = 0
+
+    def simulateSafetyDoorError(self):
+        timeInterval = random.randint(0, 20)
+        time.sleep(timeInterval)
+        raise Exception("Safety door open. Machine stopping!")
+    
+    def exceptionHandler(self, exc_type, exc_value, traceback):
+        if exc_type is not KeyboardInterrupt:
+            try:
+                self.simulateSafetyDoorError()
+            except Exception as e:
+                print("Error occurred while simulating error:", e)
+                self.stopMachine()
+                print("Machine runtime: " + str(machineSimu.getRuntime()) + " seconds.")
+
+    def startMachine(self):
+        sys.excepthook = self.exceptionHandler
+        print("Starting the Machine...")
+        self.startTime = time.time()
+        #
+        #
+        print("Machine is running.")
+    
+    def stopMachine(self):
+        print("Stopping the Machine...")
+        self.stopTime = time.time()
+        #
+        #
+        #
+        self.calculateRuntime()
+        print("Machine stopped.")
+
+    def calculateRuntime(self):
+        self.runTime = round(self.stopTime - self.startTime, 2)
+
+    def getRuntime(self):
+        self.calculateRuntime()
+        return self.runTime
+
+
+
+machineSimu = Simulator()
+machineSimu.startMachine()
+machineSimu.simulateSafetyDoorError()
+time.sleep(30)
+machineSimu.stopMachine()
+print("Machine runtime: " + str(machineSimu.getRuntime()) + " seconds.")
