1b226e74fac3a498c61475bc2d304f6f1150fc6f
Luurit00 <luurit00@hs-esslingen.de>
2023-05-02 17:44:30 +0200

    feat: added dockerfile & docker-compose for sqlite

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker-compose kill
-docker build -f backend/Dockerfile -t flask-api .
-docker build -f nginx/Dockerfile -t nginx-service .
+docker build --no-cache -f backend/Dockerfile -t flask-api .
+docker build --no-cache -f nginx/Dockerfile -t nginx-service .
+docker build --no-cache -f database/Dockerfile -t database .
 docker-compose up -d --remove-orphans
