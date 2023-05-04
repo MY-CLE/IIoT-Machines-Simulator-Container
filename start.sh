819d30ba2a8883cd9062e7d868150a2648c74f19
Malte Budig <100218844+Doderich@users.noreply.github.com>
2023-05-04 23:23:09 +0200

    fix: removed unnecessary file

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker-compose kill
-docker build --no-cache -f backend/Dockerfile -t flask-api .
-docker build --no-cache -f nginx/Dockerfile -t nginx-service .
-docker build --no-cache -f database/Dockerfile -t database .
-docker-compose up -d --remove-orphans
