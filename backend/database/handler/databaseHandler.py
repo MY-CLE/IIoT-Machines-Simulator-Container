a2288dca90f84a3e37201709c53f5c018e3adf51
Luurit00 <luurit00@hs-esslingen.de>
2023-06-08 10:44:22 +0200

    fix(database): paramterized queries

--- backend/database/handler/databaseHandler.py
+++ backend/database/handler/databaseHandler.py
@@ -4,45 +4,33 @@ import sys
 sys.path.append("../backend")
 from database.orm.databaseObject import DatabaseObject
 from database.orm.machine.machineProgram import MachineProgram
-from database.orm.program.programState import ProgramState
 from database.orm.notification.warning import Warning
 from database.orm.notification.error import Error
+from database.orm.user.admin import Admin
+from database.orm.machine.machineState import MachineState
 
 class DatabaseHandler:
 
-    #set up the connection to the database file and use a cusror for queries
     _CONNECTION = sqlite3.connect("database/machine-sim.db")
     _CURSOR = _CONNECTION.cursor()
 
-    #execute a query with the cursor and return the result set in a list
-    #close cursor when done
+
     @staticmethod
-    def select(query: str) -> list[DatabaseObject]:
+    def select(query: str, parameter: str = None) -> list[DatabaseObject]:
         DatabaseHandler._CURSOR = DatabaseHandler._CONNECTION.cursor()
-        DatabaseHandler._CURSOR.execute(query)
+        if parameter == None:
+            DatabaseHandler._CURSOR.execute(query)
+        else:
+            DatabaseHandler._CURSOR.execute(query, parameter)
         resultSet: list[DatabaseObject] = DatabaseHandler._CURSOR.fetchall()
         DatabaseHandler._CURSOR.close()
         return resultSet
-
-    #get programState out of the Database using the primary key of the table -> result is unique
-    @staticmethod
-    def selectProgramState(wId: int) -> ProgramState:
-        resultSet: list[DatabaseObject] = DatabaseHandler.select(f"SELECT * FROM program_state WHERE program_state_id = '{wId}'")
-        return ProgramState(DatabaseObject(resultSet[0]))
-
-    #get the machineProgram by name
-    #return the first result of the query
+    
     @staticmethod
     def selectMachineProgram(name: str) -> MachineProgram:
-       resultSet: list[DatabaseObject] = DatabaseHandler.select(f"SELECT * FROM machine_program WHERE machine_program_description = '{name}'")
+       resultSet: list[DatabaseObject] = DatabaseHandler.select("SELECT * FROM machine_program WHERE machine_program_description = ?", (name,))
        return MachineProgram(DatabaseObject(resultSet[0]))
     
-    @staticmethod
-    def selectMachineProgramById(id: str) -> MachineProgram:
-        resultSet: list[DatabaseObject] = DatabaseHandler.select(f"SELECT * FROM machine_program WHERE machine_prorgram_id = '{id}'")
-        return MachineProgram(DatabaseObject(resultSet[0]))
-    
-    #get all possible warning messages within the Database
     @staticmethod
     def selectWarningMessages() -> list[str]:
         resultSet: list[DatabaseObject] = DatabaseHandler.select(f"SELECT * FROM warning")
@@ -52,7 +40,6 @@ class DatabaseHandler:
             warningMessages.append(warning.getType())
         return warningMessages
     
-     #get all possible error messages within the Database
     @staticmethod
     def selectErrorMessages() -> list[str]:
         resultSet: list[DatabaseObject] = DatabaseHandler.select(f"SELECT * FROM error")
@@ -61,6 +48,27 @@ class DatabaseHandler:
             error: Error = Error(DatabaseObject(result))
             errorMessages.append(error.getType())
         return errorMessages
+    
+    @staticmethod
+    def selectAdminUsers() -> list[Admin]:
+        resultSet: list[DatabaseObject] = DatabaseHandler.select(f"SELECT * from admin")
+        adminUsers: list[Admin] = []
+        for result in resultSet:
+            adminUser: Admin = Admin(DatabaseObject(result))
+            adminUsers.append(adminUser)
+        return adminUsers
+    
+    @staticmethod 
+    def selectMachineStates() -> list[MachineState]:
+        resultSet: list[DatabaseObject] = DatabaseHandler.select(f"SELECT * from machine_state")
+        machineStates: list[MachineState] = []
+        for result in resultSet:
+            machineState: MachineState = MachineState(DatabaseObject(result))
+            machineStates.append(machineState)
+        return machineStates
+
+    
+
 
 
 
