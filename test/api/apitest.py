ae814d14116eccfebb0ee8c5c60e5c6a4f34e962
Enrico Belgiovine <disgame@gmx.de>
2023-06-20 22:52:06 +0200

    test: integration test for api

---
+++ test/api/apitest.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+
+import sys
+import sqlite3
+sys.path.append("..")
+from backend.database.handler.databaseHandler import DatabaseHandler
+from backend.app import app
+
+class TestApi(unittest.TestCase):
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
